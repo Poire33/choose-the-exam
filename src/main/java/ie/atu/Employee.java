bf2eb7a7e5e7170961d51816b7c5bd096c1acd1a
Rupert spelled with one P <g00423530@atu.ie>
2023-12-12 13:46:58 +0000

    Made employee class with getters, setters, and constructor

--- src/main/java/ie/atu/Employee.java
+++ src/main/java/ie/atu/Employee.java
@@ -1,4 +1,62 @@
 package ie.atu;
+import java.util.InputMismatchException;
 
 public class Employee {
+    private String name, employmentType, gender;
+    private int age, pps[];
+
+    public Employee(String name, String employmentType, String gender, int age, int pps[]) {
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmploymentType() {
+        return employmentType;
+    }
+
+    public void setEmploymentType(String employmentType) {
+        this.employmentType = employmentType;
+    }
+
+    public String getGender() {
+        if (gender == "Man" || gender == "Woman" || gender == "Non-binary") {
+            return gender;
+        }
+       else {
+
+        }
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public int getAge() {
+        if (age > 16 && age < 66) {
+            return age;
+        }
+        else {
+            System.out.println("Invalid entry, please enter an age between 6 and 66");
+        }
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int[] getPps() {
+        // Insert code to check number of values in pps
+        return pps;
+    }
+
+    public void setPps(int[] pps) {
+        this.pps = pps;
+    }
 }
